80958d3f77f751896878e0c62369343a71074c5f
Амир Хасанов <aworldx@gmail.com>
2018-09-16 09:48:07 +0300

    refactor test http client

--- spec/helpers/test_http_client.rb
+++ spec/helpers/test_http_client.rb
@@ -6,16 +6,16 @@ module TestHttpClient
         message: "OK"
       }
 
-      if addr.include?('geobase')
-        response.merge!(body: File.open("spec/fixtures/geo_response.xml").read)
-      elsif addr.include?('metaweather') && addr.include?('search')
-        response.merge!(body: File.open("spec/fixtures/meta_weather_city_response.json").read)
-      elsif addr.include?('metaweather')
-        response.merge!(body: File.open("spec/fixtures/meta_weather_response.json").read)
-      elsif addr.include?('openweathermap')
-        response.merge!(body: File.open("spec/fixtures/open_weather_response.json").read)
-      end
-
+      file_name = if addr.include?('geobase')
+                    "spec/fixtures/geo_response.xml"
+                  elsif addr.include?('metaweather') && addr.include?('search')
+                    "spec/fixtures/meta_weather_city_response.json"
+                  elsif addr.include?('metaweather')
+                    "spec/fixtures/meta_weather_response.json"
+                  elsif addr.include?('openweathermap')
+                    "spec/fixtures/open_weather_response.json"
+                  end
+      response.merge!(body: File.open(file_name).read)
       response
     end
   end
